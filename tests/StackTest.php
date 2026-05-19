f0ca6deb42b63e3997df0a9543987ff65f32bdb3
toni <tonigstm96@gmail.com>
2026-05-19 16:05:49 +0200

    REFACTOR: test name (peek_should_return_last_pushed_element)

--- tests/StackTest.php
+++ tests/StackTest.php
@@ -46,7 +46,7 @@ final class StackTest extends TestCase
     }
 
     #[TEST]
-    public function peek_should_return_first_pushed_element(): void
+    public function peek_should_return_last_pushed_element(): void
     {
         $stack = new Stack();
         $stack->push('hello');
