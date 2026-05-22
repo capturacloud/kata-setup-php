39ca46899741783fb4c2b9ea4ea9a5ed4233b5ea
toni <tonigstm96@gmail.com>
2026-05-22 19:16:57 +0200

    GREEN: left_should_return_t_rotated_left_when_given_t

--- tests/TetrisTest.php
+++ tests/TetrisTest.php
@@ -21,6 +21,11 @@ final class TetrisTest extends TestCase
         '#'
     ];
 
+    private const array T = [
+        '###',
+        ' # '
+    ];
+
     public function test_left_should_return_o_rotated_left_when_given_o(): void
     {
         self::assertSame(self::O, new Tetris()->left(self::O));
@@ -29,6 +34,15 @@ final class TetrisTest extends TestCase
     public function test_left_should_return_i_rotated_left_when_given_i(): void
     {
         self::assertSame(['####'], new Tetris()->left(self::I));
+    }
+
+    public function test_left_should_return_t_rotated_left_when_given_t(): void
+    {
+        self::assertSame([
+            '# ',
+            '##',
+            '# '
+        ], new Tetris()->left(self::T));
     }
 
     public function test_right_should_return_o_rotated_right_when_given_o(): void
